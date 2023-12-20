200e11f3a863f6abda211274807c7eb4164b7da4
luofu <mqa010225@163.com>
2023-12-20 13:54:20 +0800

    <feat>(<project>):<test framework>
    blog os kernel test
      - test case
      - test framework

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 #![feature(asm)]
 #![no_std]
 #![no_main]
@@ -18,4 +20,19 @@ pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
