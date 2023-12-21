09719dabb9e8ba1486e18eed6a16fe630da5d17d
luofu <mqa010225@163.com>
2023-12-21 14:22:46 +0800

    <fix>(<blog os>):<double faults>
    blog os kernel double faults
      - cargo build bugs

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -14,13 +14,12 @@ pub extern "C" fn _start() -> ! {
     blog_os::init();
 
     fn stack_overflow() {
-        stack_overflow(); // 每一次递归都会将返回地址入栈
+        stack_overflow(); // for each recursion, the return address is pushed
     }
 
-    // 触发 stack overflow
+    // uncomment line below to trigger a stack overflow
     // stack_overflow();
 
-    // as before
     #[cfg(test)]
     test_main();
 
@@ -40,9 +39,4 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
 }
